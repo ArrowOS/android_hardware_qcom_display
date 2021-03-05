6579d91e28caa076faf11a395cb3930ab0c50083
daniml3 <danimoral1001@gmail.com>
2021-03-05 18:33:46 +0100

    sdm: make sure the FOD pressed layer is correctly detected
    This behaviour can be enabled or disabled (at runtime or at build time) by setting the property debug.hwc.use_fod_index to 1 or 0 respectively.
    
    Allocating HWC for Virtual Displays leads to the configuration of the
    framebuffer target as GPU target composition type.
    
    While the GPU target layer is at the top zorder (which is most of the time), the flags and other attributes of the layer are invalid once reached SetupAtomic in DRM.
    To solve this, add a second way of detecting the FOD layer: its index.
    
    If the FOD layer is found in the HWC, a flag in the layer stack
    will be set with its index, which correctly reaches SetupAtomic.
    
    Once reached SetupAtomic, a check will be made for detecting the FOD flag. If the FOD flag exists in some of ther layers, then that will be the method for detecting the layer. Otherwise, the index will be used for detecting the layer, if the property is 1.
    
    For getting the FOD layer index, discard all the non-hw layers below the FOD layer so it is correctly detected at SetupAtomic. Also discard the GPU target layer from the index (if existing).
    
    Signed-off-by: daniml3 <danimoral1001@gmail.com>
    Change-Id: Icc2233909218a5569a41065bb69e8d22c99497af

--- sdm/libs/hwc2/hwc_display.cpp
+++ sdm/libs/hwc2/hwc_display.cpp
@@ -649,6 +649,29 @@ HWC2::Error HWCDisplay::DestroyLayer(hwc2_layer_t layer_id) {
   return HWC2::Error::None;
 }
 
+#ifdef FOD_ZPOS
+// From a HWC layer index, get the index that the layer will have
+// as a hardware layer
+int HWCDisplay::GetHWLayerIndex(int index) {
+    int i = 0;
+
+    // Reduce the index if the client target is the GPU target
+    if (client_target_->GetSDMLayer()->composition == kCompositionGPUTarget) {
+        index--;
+    }
+
+    for (auto hwc_layer : layer_set_) {
+        Layer *layer = hwc_layer->GetSDMLayer();
+        // Reduce the index if there are non-hw layers below the layer
+        if (layer->composition != kCompositionSDE && index > i) {
+            index--;
+        }
+        i++;
+    }
+
+    return index;
+}
+#endif
 
 void HWCDisplay::BuildLayerStack() {
   layer_stack_ = LayerStack();
@@ -674,12 +697,6 @@ void HWCDisplay::BuildLayerStack() {
     } else if (hwc_layer->GetClientRequestedCompositionType() == HWC2::Composition::SolidColor) {
       layer->flags.solid_fill = true;
     }
-
-#ifdef FOD_ZPOS
-    if (hwc_layer->IsFodPressed()) {
-      layer->flags.fod_pressed = true;
-    }
-#endif
 
     if (!hwc_layer->IsDataSpaceSupported()) {
       layer->flags.skip = true;
@@ -1311,6 +1328,7 @@ DisplayError HWCDisplay::HandleEvent(DisplayEvent event) {
 }
 
 HWC2::Error HWCDisplay::PrepareLayerStack(uint32_t *out_num_types, uint32_t *out_num_requests) {
+  int i = 0;
   layer_changes_.clear();
   layer_requests_.clear();
   has_client_composition_ = false;
@@ -1380,6 +1398,13 @@ HWC2::Error HWCDisplay::PrepareLayerStack(uint32_t *out_num_types, uint32_t *out
       layer_changes_[hwc_layer->GetId()] = device_composition;
     }
     hwc_layer->ResetValidation();
+#ifdef FOD_ZPOS
+    if (hwc_layer->IsFodPressed()) {
+      layer_stack_.fod_layer_index = GetHWLayerIndex(i);
+      layer->flags.fod_pressed = true;
+    }
+#endif
+    i++;
   }
 
   if ((has_client_composition_) && (!has_force_client_composition_)) {
